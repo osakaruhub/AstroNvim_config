34e61d3d7c452117d6ead1017345a71639af2872
osakaruhub <69129322+osakaruhub@users.noreply.github.com>
2023-09-18 08:17:35 +0200

    "second fix"

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,5 +8,63 @@ return {
   --   config = function()
   --     require("lsp_signature").setup()
   --   end,
+   {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    cmd = {"TodoQuickFix"},
+    keys = {
+      { "<Leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope"},
+    },
+  },
+  { -- scientific annotation
+    'jbyuki/nabla.nvim',
+  },
+  { -- tagbar
+    'majutsushi/tagbar'
+  },
+  {
+    'jose-elias-alvarez/null-ls.nvim',
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  { -- nvim-jdtls
+    'mfussenegger/nvim-jdtls',
+  },
+  { -- feline (statusbar)
+    'feline-nvim/feline.nvim',
+    config = function()
+      require('feline').setup()
+    end,
+  },
+  { -- screensaver
+    "folke/drop.nvim",
+    event = "VimEnter",
+    config = function()
+      require("drop").setup()
+    end
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    },
+  },
+  { -- discord Rich-Presence
+    'andweeb/presence.nvim',
+    opts = {},
+    event = "VeryLazy",
+  },
+  { -- learn neovim  
+     "m4xshen/hardtime.nvim",
+     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+     opts = {}
+  },
   -- },
 }
